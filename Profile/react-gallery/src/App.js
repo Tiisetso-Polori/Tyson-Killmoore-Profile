42c10fb7af0c10fc5459a1c42022a85996442257
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2023-06-21 09:30:34 +0200

    Added Basic React Gallery Landing Page

---
+++ Profile/react-gallery/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign: 'center'}}>Killmoore Gallery</h1>
+    <Gallery />
+    </>
+    );
+}
+
+export default App;
+
